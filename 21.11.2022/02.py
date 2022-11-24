c8d0421caaea51e4e4a3a7b30213e170f9e27723
m_a_g_ <m_a_g_12378@mail.ru>
2022-11-24 09:03:59 +0500

    Задачи первого семинара готовы

--- 21.11.2022/02.py
+++ 21.11.2022/02.py
@@ -2,17 +2,23 @@
 # Примеры:    
 # - 1, 4, 8, 7, 5 -> 8
 # - 78, 55, 36, 90, 2 -> 90
+print("\033[H\033[J")
 print('Введите числа')
-a = int(input('a= '))
-b = int(input('b= '))
-c = int(input('c= '))
-d = int(input('d= '))
-e = int(input('e= '))
+a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+d = int(input('d = '))
+e = int(input('e = '))
 numbers = [a, b, c, d, e]
 print (numbers)
 
 max = numbers[0]
-for i in range(numbers):
+n_max = 0
+for i in range(5):
     if numbers[i] > max:
         max = numbers[i]
-
+        n_max = i
+        i = i + 1
+    else:
+        i = i + 1
+print (f'Максимальное число в массиве {max}, элемент в массиве номер: {n_max+1}')
