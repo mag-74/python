c199c9ad9d023ee83ad0ce6976bfd69e02a413d4
m_a_g_ <m_a_g_12378@mail.ru>
2022-11-25 21:38:09 +0500

    Еще разок

--- 21.11.2022_DZ/02.py
+++ 21.11.2022_DZ/02.py
@@ -1,7 +1,20 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 print("\033[H\033[J")
-x = bool(False)
-y = bool(False)
-z = bool(False)
-#for i = 
-#print(f'Значения: {x}, {y}, {z}')
+
+print(f'x\t y\t z\t ¬(X ⋁ Y ⋁ Z)\t ¬X ⋀ ¬Y ⋀ ¬Z\t Проверка')
+for i in range(2):
+    x = bool(i)
+    i +=1
+    for j in range(2):
+        y = bool(j)
+        j +=1
+        for k in range(2):
+            z = bool(k)
+            k +=1
+            expression_1 = bool(not(x and y and z))
+            expression_2 = bool(not(x) or not(y) or not(z))
+            if expression_1 == expression_2:
+                comparison = bool(True)
+            else:
+                comparison = bool(False)
+            print(f'{x}\t {y}\t {z}\t {expression_1}\t\t {expression_2}\t\t {comparison}')
